95193aafa6422ba3048f5e195a2f29b33d35bc9d
username <email>
2019-02-12 15:54:07 -0800

    Complete TicTacToe game

---
+++ src/ttt-view.js
@@ -0,0 +1,56 @@
+class View {
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+
+    this.setupBoard();
+    this.bindEvents();
+    
+  }
+
+  bindEvents() {
+    console.log(this.game.isOver());
+    this.$el.on('click', '.black', ( event => {
+      const $square = $(event.target);
+      console.log($square);
+      this.makeMove($square);
+    }));
+  }
+
+  makeMove($square) {
+    if (this.game.isOver() === false) {
+      if (this.game.currentPlayer === 'x') {
+        $square.removeClass('black');
+        $square.addClass('x');
+      } else {
+        $square.removeClass('black');
+        $square.addClass('o');
+      }
+      $square.html(`${this.game.currentPlayer}`);
+      
+      let pos = JSON.parse($square.attr('id'));
+      this.game.playMove(pos);
+
+
+      if (this.game.isOver()) {
+        alert(`GAME OVER ${this.game.winner()} WINS!!!!!`);
+      }
+    }
+  }
+
+  setupBoard() {
+    const $board = $('<ul>');
+    console.log($board);
+    $board.addClass('squares');
+
+    for (let i = 0; i < 9; i++) {
+      let $square = $("<li>");
+      $square.addClass('black');
+      $square.attr('id', `[${Math.floor(i/3)},${i%3}]`)
+      $board.append($square);
+    }
+    this.$el.append($board);
+  }
+}
+
+module.exports = View;
